e454190407a8ea01e1dd1c5e84ab87e8737b7aab
bum1988 <bum1988@naver.com>
2019-03-14 11:15:23 +0900

    no message

--- src/test/java/com/jojoldu/webservice/web/WebControllerTest.java
+++ src/test/java/com/jojoldu/webservice/web/WebControllerTest.java
@@ -10,7 +10,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-
+/**
+ * Created by jojoldu@gmail.com on 2017. 12. 31.
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -19,5 +23,12 @@ public class WebControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Test
+    public void 메인페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
 
+        //then
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
+    }
 }

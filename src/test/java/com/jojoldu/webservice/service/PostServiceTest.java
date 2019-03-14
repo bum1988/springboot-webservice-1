42eb9e13af62a7f4b24079589d57b2745254c7f7
bum1988 <bum1988@naver.com>
2019-03-14 11:02:31 +0900

    no message

--- src/test/java/com/jojoldu/webservice/service/PostServiceTest.java
+++ src/test/java/com/jojoldu/webservice/service/PostServiceTest.java
@@ -13,11 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-/**
- * Created by jojoldu@gmail.com on 2018. 1. 1.
- * Blog : http://jojoldu.tistory.com
- * Github : https://github.com/jojoldu
- */
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -29,27 +25,5 @@ public class PostServiceTest {
     @Autowired
     private PostsRepository postsRepository;
 
-    @After
-    public void cleanup () {
-        postsRepository.deleteAll();
-    }
 
-    @Test
-    public void Dto데이터가_posts테이블에_저장된다 () {
-        //given
-        PostsSaveRequestDto dto = PostsSaveRequestDto.builder()
-                .author("jojoldu@gmail.com")
-                .content("테스트")
-                .title("테스트 타이틀")
-                .build();
-
-        //when
-        postsService.save(dto);
-
-        //then
-        Posts posts = postsRepository.findAll().get(0);
-        assertThat(posts.getAuthor()).isEqualTo(dto.getAuthor());
-        assertThat(posts.getContent()).isEqualTo(dto.getContent());
-        assertThat(posts.getTitle()).isEqualTo(dto.getTitle());
-    }
 }
